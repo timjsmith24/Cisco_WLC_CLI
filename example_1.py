66fe950f08f8f7efe7890f0564b33200e0b2ffe3
Tim Smith <timjsmith24@MacBook-Pro.localdomain>
2019-09-20 22:06:28 -0400

    example_1.py

--- example_1.py
+++ example_1.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
+import os
 import getpass
 from netmiko import Netmiko
 from netmiko.ssh_exception import NetMikoAuthenticationException, NetMikoTimeoutException
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+os.environ["NET_TEXTFSM"]='{}/ntc-templates/templates/'.format(PATH)
+
 
 host = '192.168.1.11'
 username = input('please enter the username: ')
@@ -15,17 +22,17 @@ try:
 except (NetMikoAuthenticationException, NetMikoTimeoutException):
     print('Failed to connect to {}'.format(host))
     exit()
-
-output = netconnect.send_command('show sysinfo')
+'''
+output = netconnect.send_command('show sysinfo', use_textfsm=True)
 print(output)
-
-cmd = ‘show inventory’
-output = netconnect.send_command(cmd)
+'''
+cmd = 'show inventory'
+output = netconnect.send_command(cmd, use_textfsm=True)
 print(output)
-
+'''
 cmds = ['show sysinfo','show inventory']
 for cmd in cmds:
 	output = netconnect.send_command(cmd)
 	print(output)
-	
+'''	
 netconnect.disconnect()
